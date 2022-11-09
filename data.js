102f24cf04498cc0b081fdd5beac89eda9dc552d
N0i7 <105717953+N0i7@users.noreply.github.com>
2022-11-09 13:24:44 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+[
+{
+"firstName": "Leo",
+"lastName": "S",
+"age": 30,
+},
+{
+"firstName": "Ann",
+"lastName": "X",
+"age": 30,
+},
+{
+"firstName": "Rami",
+"lastName": "Q",
+"age": 30,
+},
+{
+"firstName": "Flo",
+"lastName": "D",
+"age": 30,
+},
+{
+"firstName": "Giaco",
+"lastName": "F",
+"age": 30,
+},
+]
